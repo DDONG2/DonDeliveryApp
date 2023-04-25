6cbd19587283e3b5b1073faea1178fd9ffb83d08
DoEun Lee <>
2023-04-25 17:01:10 +0900

    fix : deprecated 바텀 네비 로직 변경

--- app/src/main/java/com/example/dondeliveryapp/presenter/main/MainActivity.kt
+++ app/src/main/java/com/example/dondeliveryapp/presenter/main/MainActivity.kt
@@ -2,15 +2,13 @@ package com.example.dondeliveryapp.presenter.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.example.dondeliveryapp.R
 import com.example.dondeliveryapp.databinding.ActivityMainBinding
 import com.example.dondeliveryapp.presenter.main.home.HomeFragment
 import com.example.dondeliveryapp.presenter.main.my.MyFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -21,39 +19,32 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setContentView(binding.root)
 
         initViews()
-
-
-    }
-     fun initViews() = with(binding) {
-        bottomNav.setOnNavigationItemSelectedListener(this@MainActivity)
-         showFragment(HomeFragment.newInstance(), HomeFragment.TAG)
+        
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.menu_home -> {
-                showFragment(HomeFragment.newInstance(), HomeFragment.TAG)
-                true
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun initViews() = with(binding) {
+        replaceFragment(HomeFragment())
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.myFragment -> {
+                    replaceFragment(MyFragment())
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> return@setOnItemSelectedListener false
             }
-            R.id.menu_my -> {
-                showFragment(MyFragment.newInstance(), MyFragment.TAG)
-                true
-            }
-            else -> false
-        }
-    }
 
-    private fun showFragment(fragment: Fragment, tag: String) {
-        val findFragment = supportFragmentManager.findFragmentByTag(tag)
-        supportFragmentManager.fragments.forEach { fm ->
-            supportFragmentManager.beginTransaction().hide(fm).commitAllowingStateLoss()
-        }
-        findFragment?.let {
-            supportFragmentManager.beginTransaction().show(it).commitAllowingStateLoss()
-        } ?: kotlin.run {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainer, fragment, tag)
-                .commitAllowingStateLoss()
         }
     }
 
